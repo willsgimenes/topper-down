aa7b508d2d6ee49f443e844c62da2e078993f7eb
willsgimenes <willsgimenes@gmail.com>
2020-04-11 22:50:07 -0300

    adding tiled maps using super tile map unit

---
+++ Assets/Tiled Maps/oveworld.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="oveworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <editorsettings>
+  <export target="oveworld..tsx" format="tsx"/>
+ </editorsettings>
+ <image source="Art/gfx/Overworld.png" width="640" height="576"/>
+ <terraintypes>
+  <terrain name="Path" tile="-1"/>
+ </terraintypes>
+ <tile id="1160" terrain=",,,0"/>
+ <tile id="1161" terrain=",,0,0"/>
+ <tile id="1162" terrain=",,0,"/>
+ <tile id="1200" terrain=",0,,0"/>
+ <tile id="1201" terrain="0,0,0,0"/>
+ <tile id="1202" terrain="0,,0,"/>
+ <tile id="1240" terrain=",0,,"/>
+ <tile id="1241" terrain="0,0,,"/>
+ <tile id="1242" terrain="0,,,"/>
+ <tile id="1280" terrain="0,0,0,"/>
+ <tile id="1281" terrain="0,0,,0"/>
+ <tile id="1320" terrain="0,,0,0"/>
+ <tile id="1321" terrain=",0,0,0"/>
+</tileset>
